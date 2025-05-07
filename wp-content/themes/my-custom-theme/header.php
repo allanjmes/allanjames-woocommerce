8d1a14c492dec5a39680c5ec25b2a953fc8f0239
allanjmes <allanjames.regala@gmail.com>
2025-05-08 07:32:13 +0800

    Add tailwind theme colors

--- wp-content/themes/my-custom-theme/header.php
+++ wp-content/themes/my-custom-theme/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class('bg-secondary'); ?>>
 
 <?php
     require get_template_directory() . '/src/global/header.php';
