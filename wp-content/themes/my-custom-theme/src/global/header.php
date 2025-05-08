627a905db690745230ad726292651140190ccdf7
allanjmes <allanjames.regala@gmail.com>
2025-05-08 16:05:01 +0800

    Add header

--- wp-content/themes/my-custom-theme/src/global/header.php
+++ wp-content/themes/my-custom-theme/src/global/header.php
@@ -1,1 +1,42 @@
-<h1>this is header</h1>
+<header class="mx-5">
+    <div class="flex justify-between py-5 border-b border-primary">
+        <div class="flex gap-5 items-center">
+            <button class="btn-outline-primary flex gap-1 group/svg-menu">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20px"
+                    height="20px"
+                    viewBox="0 0 1024 1024"
+                    class="icon fill-primary group-hover/svg-menu:fill-secondary"
+                    version="1.1"
+                >
+                    <path d="M109.632 673.664h519.68c25.152 0 45.568-22.016 45.568-48.896 0-26.88-20.416-48.896-45.568-48.896h-519.68c-25.216 0-45.632 22.016-45.632 48.896 0 26.88 20.48 48.896 45.632 48.896z m0-228.096h519.68c25.152 0 45.568-21.952 45.568-48.896 0-26.88-20.416-48.896-45.568-48.896h-519.68c-25.216 0-45.632 22.016-45.632 48.896 0 26.88 20.48 48.896 45.632 48.896z m3.264-219.904h795.776c26.88 0 50.56-20.352 51.328-47.168A48.896 48.896 0 0 0 911.104 128H115.328c-26.88 0-50.56 20.416-51.328 47.168a48.896 48.896 0 0 0 48.896 50.56z m619.776 447.232V348.672L960 510.784l-227.328 162.112c0 0.768 0 0.768 0 0z m178.432 122.944H115.328c-26.88 0-50.56 20.48-51.328 47.232a48.896 48.896 0 0 0 48.896 50.496h795.776c26.88 0 50.56-20.416 51.328-47.232a48.896 48.896 0 0 0-48.896-50.496z"/>
+                </svg>
+                Menu
+            </button>
+            <button class="btn-outline-primary flex gap-1 group/svg-search">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20px"
+                    height="20px"
+                    viewBox="0 0 24 24"
+                    class="icon
+                        stroke-primary group-hover/svg-search:stroke-secondary
+                        fill-secondary group-hover/svg-search:fill-primary"
+                >
+                    <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                Search
+            </button>
+        </div>
+        <div>
+            <h1>Logo</h1>
+        </div>
+        <div class="flex gap-5 items-center">
+            <button id="toggle-theme-btn" class="btn-primary">
+                <h1>Toggle</h1>
+            </button>
+            <h1 class="text-primary">Profile</h1>
+        </div>
+    </div>
+</header>
